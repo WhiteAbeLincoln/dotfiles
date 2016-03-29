adeddb9f21a690dede03dee533195b8b165f11b1
Abraham White <abelincoln.white@gmail.com>
2016-03-29 01:44:42 -0600

    finished basic lemonbar

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -46,8 +46,7 @@ myManageHook = composeAll
     , className =? "google-chrome"  --> doShift "2:web"
     , className =? "Gedit"          --> doShift "3:editor"
     , className =? "MultiMC5"       --> doShift "4:games"
-    , className =? "Steam"          --> doFloat <+> doShift "4:games"
-    , className =? "hl2_linux"      --> doFullFloat <+> doShift "4:games"
+    , className =? "Steam"          --> doShift "4:games"
     , className =? "Spotify"        --> doShift "5:media"
     --, name      =? "Netflix"        --> doShift "5:media"
     , isFullscreen                  --> myDoFullFloat
@@ -60,8 +59,6 @@ myManageHook = composeAll
 myNormalBorderColor = "#555555"
 myFocusedBorderColor = "#dab3af"
 
-xmobarTitleColor = "#dab3af"
-xmobarCurrentWorkspaceColor = "#aec29b"
 myBorderWidth = 1
 
 -------------------------------------------------------------------------------
@@ -89,54 +86,31 @@ myLayout = named "S_BSP" myBSP ||| emptyBSP ||| named "S_Tall" spaced ||| tall |
         -- percent of screen to increment by when resizing panes
         delta = 1/100
 
-
-
-main = xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
-
-myPP = xmobarPP { ppTitle = xmobarColor xmobarTitleColor "" . shorten 100 -- sets the title color and shortens at 100 chars
-                , ppCurrent = xmobarColor xmobarCurrentWorkspaceColor ""
-                , ppVisible = wrap "{" "}"
-                }
-
-toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
 ctrlMask = controlMask
 altMask = mod1Mask
 
-myConfig = ewmh defaultConfig
-    { terminal           = myTerminal
-    , modMask            = myModMask
-    , workspaces         = myWorkspaces
-    , normalBorderColor  = myNormalBorderColor
-    , focusedBorderColor = myFocusedBorderColor
-    , borderWidth        = myBorderWidth
-    , manageHook         = myManageHook
-    , layoutHook         = avoidStruts $ smartBorders $ myLayout
-    -- fix for double tap avoid struts key
-    , handleEventHook    = docksEventHook <+> fullscreenEventHook <+> handleEventHook defaultConfig
-    } `additionalKeys`
-    [ ((0 , xF86XK_AudioLowerVolume    ),  spawn "pulseaudio-ctl down")
-    , ((0 , xF86XK_AudioRaiseVolume    ),  spawn "pulseaudio-ctl up")
-    , ((0 , xF86XK_AudioMute           ),  spawn "pulseaudio-ctl mute")
-    , ((mod4Mask .|. controlMask, xK_l ), spawn myScreensaver)
-    , ((mod4Mask,                 xK_p ), spawn "rofi -show run")
-    , ((mod4Mask .|. shiftMask,   xK_p ), spawn "j4-dmenu-desktop --dmenu='rofi -dmenu'")
-    , ((mod4Mask,               xK_Tab ), spawn "rofi -show window")
-    , ((mod4Mask,               xK_s   ), spawn "rofi -show ssh")
-    , ((mod4Mask, xK_g), goToSelected defaultGSConfig)
-    -- BSP Keys
-    {-
-    , ((myModMask,               xK_l     ), sendMessage $ ExpandTowards R)
-    , ((myModMask,               xK_h     ), sendMessage $ ExpandTowards L)
-    , ((myModMask .|. altMask,               xK_j     ), sendMessage $ ExpandTowards D)
-    , ((myModMask .|. altMask,               xK_k     ), sendMessage $ ExpandTowards U)
-    , ((myModMask .|. altMask .|. ctrlMask , xK_l     ), sendMessage $ ShrinkFrom R)
-    , ((myModMask .|. altMask .|. ctrlMask , xK_h     ), sendMessage $ ShrinkFrom L)
-    , ((myModMask .|. altMask .|. ctrlMask , xK_j     ), sendMessage $ ShrinkFrom D)
-    , ((myModMask .|. altMask .|. ctrlMask , xK_k     ), sendMessage $ ShrinkFrom U)
-    , ((myModMask,                           xK_r     ), sendMessage Rotate)
-    , ((myModMask,                           xK_s     ), sendMessage Swap)
-    , ((myModMask,                           xK_n     ), sendMessage FocusParent)
-    , ((myModMask .|. ctrlMask,              xK_n     ), sendMessage SelectNode)
-    , ((myModMask .|. shiftMask,             xK_n     ), sendMessage MoveNode)
-    -}
-    ]
+main = do
+    lemonbar <- spawn "barrun"
+    xmonad $ ewmh defaultConfig
+        { terminal           = myTerminal
+        , modMask            = myModMask
+        , workspaces         = myWorkspaces
+        , normalBorderColor  = myNormalBorderColor
+        , focusedBorderColor = myFocusedBorderColor
+        , borderWidth        = myBorderWidth
+        , manageHook         = myManageHook
+        , layoutHook         = avoidStruts $ smartBorders $ myLayout
+        -- fix for double tap avoid struts key
+        , handleEventHook    = docksEventHook <+> fullscreenEventHook <+> handleEventHook defaultConfig
+        } `additionalKeys`
+        [ ((0 , xF86XK_AudioLowerVolume    ),  spawn "pulseaudio-ctl down")
+        , ((0 , xF86XK_AudioRaiseVolume    ),  spawn "pulseaudio-ctl up")
+        , ((0 , xF86XK_AudioMute           ),  spawn "pulseaudio-ctl mute")
+        , ((mod4Mask .|. controlMask, xK_l ), spawn myScreensaver)
+        , ((mod4Mask,                 xK_p ), spawn "rofi -show run")
+        , ((mod4Mask .|. shiftMask,   xK_p ), spawn "j4-dmenu-desktop --dmenu='rofi -dmenu'")
+        , ((mod4Mask,               xK_Tab ), spawn "rofi -show window")
+        , ((mod4Mask,               xK_s   ), spawn "rofi -show ssh")
+        , ((mod4Mask,               xK_o   ), spawn "switcher.sh")
+        , ((mod4Mask, xK_g), goToSelected defaultGSConfig)
+        ]
