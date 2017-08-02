15ddbb3b484467801e61a546ffbf3353bce2ce34
Abraham White <abe@abrahamwhite.com>
2017-08-02 09:02:37 -0700

    Update xmonad config

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -1,30 +1,43 @@
-import XMonad
+{-# LANGUAGE ExistentialQuantification #-}
+import XMonad hiding ((|||))
 import qualified XMonad.StackSet as W
 import Data.Maybe (fromMaybe)
-import XMonad.Layout.NoBorders
-import XMonad.Actions.SpawnOn
-import XMonad.Actions.GridSelect
-import XMonad.Hooks.ManageDocks
-import XMonad.Hooks.ManageHelpers
-import XMonad.Hooks.EwmhDesktops
+import XMonad.Layout.NoBorders (smartBorders)
+import XMonad.Layout.BinarySpacePartition
+import XMonad.Layout.BorderResize
+import XMonad.Layout.LayoutCombinators
+import XMonad.Layout.Renamed
+import XMonad.Layout.Spacing
+import XMonad.Actions.SpawnOn (spawnOn)
+import XMonad.Actions.Navigation2D ( navigation2D, windowGo, windowSwap)
+import XMonad.Hooks.ManageDocks (manageDocks, avoidStruts, docks)
+import XMonad.Hooks.ManageHelpers (isFullscreen, doFullFloat)
+import XMonad.Hooks.EwmhDesktops (fullscreenEventHook, ewmh)
+import XMonad.Hooks.DynamicLog
 import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Util.SpawnOnce (spawnOnce)
+import XMonad.Util.Dmenu
+import XMonad.Util.Run
 import Graphics.X11.ExtraTypes.XF86
-import System.IO()
-import XMonad.Config
-import XMonad.Actions.Commands
+import XMonad.Config (def)
+import XMonad.Actions.Commands (defaultCommands, runCommand)
+import Data.Monoid (Endo(..))
 
 -----------------------------------------------------------------------------
 -- Functions
 --
-strip ys = filter $ not . (`elem` ys)
--- src <- readFile "/home/abe/.shell-themes/.current_theme"
+-- strip :: (Eq a, Foldable t) => t a -> [a] -> [a]
+-- strip ys = filter $ not . (`elem` ys)
 
 -----------------------------------------------------------------------------
 -- Customized programs
 --
 -- myScreensaver = "/usr/bin/xautolock -locknow"
+myScreensaver :: [Char]
 myScreensaver = "light-locker-command -l"
+toggleScreensaver :: [Char]
 toggleScreensaver = "/usr/bin/xautolock -toggle"
+myTerminal :: [Char]
 myTerminal = "termite -e /usr/bin/tmux"
 
 ------------------------------------------------------------------------------
@@ -36,6 +49,7 @@ workspaceMap = [("term", "\xf120"), ("web", "\xf269"), ("editor", "\xf121"), ("g
 getWorkspace :: String -> String
 getWorkspace = fromMaybe "9" . flip lookup workspaceMap
 
+myWorkspaces :: [String]
 myWorkspaces = foldr (\x acc -> snd x:acc) [] workspaceMap ++ map show [(length workspaceMap + 1)..9]
 
 ------------------------------------------------------------------------------
@@ -45,6 +59,7 @@ myWorkspaces = foldr (\x acc -> snd x:acc) [] workspaceMap ++ map show [(length 
 myDoFullFloat :: ManageHook
 myDoFullFloat = doF W.focusDown <+> doFullFloat
 
+myManageHook :: Query (Endo WindowSet)
 myManageHook = composeAll
     [ className =? "Termite"        --> doShift (getWorkspace "term")
     , className =? "Firefox"        --> doShift (getWorkspace "web")
@@ -72,21 +87,50 @@ myNormalBorderColor = "#555555"
 myFocusedBorderColor :: String
 myFocusedBorderColor = "#dab3af"
 
+myBorderWidth :: Dimension
 myBorderWidth = 1
 
 -------------------------------------------------------------------------------
 -- Key Bindings
 --
-myModMask = mod4Mask -- changes mod key to super
+modm :: KeyMask
+modm = mod4Mask -- changes mod key to super
+
+altMask :: KeyMask
+altMask = mod1Mask
 
 ------------------------------------------------------------------------------
 -- Layouts
 --
-myLayout =  tall ||| Mirror tall ||| Full
-    where
-        -- default tiling algorithm partitions the screen into 2 panes
-        tall = Tall nmaster delta ratio
+jumpLayout = do
+  r <- dmenu $ layoutNames ++ gappedNames
+  sendMessage $ JumpToLayout r
+  where gappedNames = (++ "-gap") <$> layoutNames
 
+layoutNames = ["bsp", "tall", "mirror-tall", "full"]
+
+myLayout =    layouts ||| gapped
+-- for some reason, this doesn't work
+-- I can still switch to next layout, but jumpLayout doesn't seem to do anything
+-- when a gapped layout is selected
+-- layouts ||| gapped
+-- gapped = renamed [Chain [CutWordsLeft 2, Append "-gap"]] $ smartSpacingWithEdge 10 layouts
+    where
+        layouts = bsp ||| tall ||| mirrorTall ||| full
+        gapped = bspgap ||| tallgap ||| mirrorTallGap ||| fullgap
+        bsp  = renamed [Replace "bsp"] emptyBSP
+        tall = renamed [Replace "tall"] $ Tall nmaster delta ratio
+        mirrorTall = renamed [Replace "mirror-tall"] $ Mirror tall
+        full = renamed [Replace "full"] Full
+        bspgap = renamed [Replace "bsp-gap"] $ space gap emptyBSP
+        tallgap = renamed [Replace "tall-gap"] $ space gap $ Tall nmaster delta ratio
+        mirrorTallGap = renamed [Replace "mirror-tall-gap"] $ space gap $ Mirror tall
+        fullgap = renamed [Replace "full-gap"] $ space gap Full
+
+
+        space = smartSpacingWithEdge
+        gap = 10
+        -- default tiling algorithm partitions the screen into 2 panes
         -- default number of windows in the master pane
         nmaster = 1
 
@@ -96,50 +140,75 @@ myLayout =  tall ||| Mirror tall ||| Full
         -- percent of screen to increment by when resizing panes
         delta = 1/100
 
-commands :: X [(String, X ())]
-commands = defaultCommands
+makeGapped = undefined
+
+-- switchLayout = do
+
 
 startup :: X()
 startup = do
     spawnOn (getWorkspace "term") myTerminal
     spawnOn (getWorkspace "web") "vivaldi-stable"
     spawnOn (getWorkspace "media") "gpmdp"
-    spawn "/home/abe/bin/xmonad-autorun"
+    spawnOnce "/home/abe/bin/xmonad-autorun"
 
 main :: IO()
 main = do
-    -- h <- spawnPipe "nc -l localhost 1234"
-    xmonad $ ewmh XMonad.Config.def
+    xmonad
+      $ ewmh
+      $ docks
+      $ navigation2D def (xK_w, xK_a, xK_s, xK_d) [(mod4Mask, windowGo), (mod4Mask .|. shiftMask, windowSwap)] False
+      $ def
         { terminal           = myTerminal
-        , modMask            = myModMask
+        , modMask            = modm
         , workspaces         = myWorkspaces
         , normalBorderColor  = myNormalBorderColor
         , focusedBorderColor = myFocusedBorderColor
         , borderWidth        = myBorderWidth
         , manageHook         = myManageHook
         , layoutHook         = avoidStruts $ smartBorders myLayout
-        , startupHook        = startup
         -- , logHook            = dynamicLogWithPP $ XMonad.Hooks.DynamicLog.def { ppOutput = hPutStrLn h }
-        -- fix for double tap avoid struts key
-        , handleEventHook    = docksEventHook <+> fullscreenEventHook <+> handleEventHook XMonad.Config.def
+        -- , startupHook        = startup
+        , handleEventHook    = fullscreenEventHook <+> handleEventHook def
         } `additionalKeys`
-        [ ((0 , xF86XK_AudioLowerVolume    ),  spawn "ponymix -N decrease 2")
-        , ((0 , xF86XK_AudioRaiseVolume    ),  spawn "ponymix -N increase 2")
-        , ((0 , xF86XK_AudioMute           ),  spawn "ponymix -N toggle")
-        , ((0 , xF86XK_AudioPlay           ),  spawn "playerctl play-pause")
-        , ((0 , xF86XK_AudioNext           ),  spawn "playerctl next")
-        , ((0 , xF86XK_AudioPrev           ),  spawn "playerctl previous")
-        , ((0 , xF86XK_Forward             ),  spawn "playerctl next")
-        , ((0 , xF86XK_Back                ),  spawn "playerctl previous")
-        , ((0 , xF86XK_MonBrightnessUp     ),  spawn "xbacklight -inc 10")
-        , ((0 , xF86XK_MonBrightnessDown   ),  spawn "xbacklight -dec 10")
-        , ((mod4Mask .|. controlMask, xK_l ), spawn myScreensaver)
-        , ((mod4Mask .|. controlMask, xK_c ), spawn toggleScreensaver)
-        , ((mod4Mask,                 xK_y ), commands >>= runCommand)
-        , ((mod4Mask,                 xK_p ), spawn "rofi -show run")
-        , ((mod4Mask,               xK_o   ), spawn "~/bin/themer")
-        , ((mod4Mask .|. shiftMask,   xK_p ), spawn "j4-dmenu-desktop --dmenu='rofi -dmenu'")
-        , ((mod4Mask,               xK_Tab ), spawn "rofi -show window")
-        , ((mod4Mask,               xK_s   ), spawn "rofi -show ssh")
-        , ((mod4Mask, xK_g), goToSelected XMonad.Actions.GridSelect.def)
+        [ ((0 , xF86XK_AudioLowerVolume     ),  spawn "ponymix -N decrease 2")
+        , ((0 , xF86XK_AudioRaiseVolume     ),  spawn "ponymix -N increase 2")
+        , ((0 , xF86XK_AudioMute            ),  spawn "ponymix -N toggle")
+        , ((0 , xF86XK_AudioPlay            ),  spawn "playerctl play-pause")
+        , ((0 , xF86XK_AudioNext            ),  spawn "playerctl next")
+        , ((0 , xF86XK_AudioPrev            ),  spawn "playerctl previous")
+        , ((0 , xF86XK_Forward              ),  spawn "playerctl next")
+        , ((0 , xF86XK_Back                 ),  spawn "playerctl previous")
+        , ((0 , xF86XK_MonBrightnessUp      ),  spawn "xbacklight -inc 10")
+        , ((0 , xF86XK_MonBrightnessDown    ),  spawn "xbacklight -dec 10")
+        , ((modm .|. controlMask,     xK_l  ),  spawn myScreensaver)
+        , ((modm .|. controlMask,     xK_c  ),  spawn toggleScreensaver)
+        , ((modm,                     xK_y  ),  defaultCommands >>= runCommand)
+        , ((modm,                     xK_o  ),  spawn "~/bin/themer")
+        , ((modm .|. shiftMask,       xK_p  ),  spawn "j4-dmenu-desktop --dmenu='rofi -dmenu'")
+        , ((modm,                     xK_p  ),  spawn "rofi -show run")
+        , ((modm,                   xK_Tab  ),  spawn "rofi -show window")
+        , ((modm,                     xK_s  ),  spawn "rofi -show ssh")
+        , ((modm,                     xK_i  ),  jumpLayout)
+        -- BSP
+        , ((modm .|. altMask,                 xK_l     ), sendMessage $ ExpandTowards R)
+        , ((modm .|. altMask,                 xK_h     ), sendMessage $ ExpandTowards L)
+        , ((modm .|. altMask,                 xK_j     ), sendMessage $ ExpandTowards D)
+        , ((modm .|. altMask,                 xK_k     ), sendMessage $ ExpandTowards U)
+        , ((modm .|. altMask .|. controlMask, xK_l     ), sendMessage $ ShrinkFrom R)
+        , ((modm .|. altMask .|. controlMask, xK_h     ), sendMessage $ ShrinkFrom L)
+        , ((modm .|. altMask .|. controlMask, xK_j     ), sendMessage $ ShrinkFrom D)
+        , ((modm .|. altMask .|. controlMask, xK_k     ), sendMessage $ ShrinkFrom U)
+        , ((modm,                             xK_r     ), sendMessage Rotate)
+        , ((modm,                             xK_s     ), sendMessage Swap)
+        , ((modm,                             xK_n     ), sendMessage FocusParent)
+        , ((modm .|. controlMask,             xK_n     ), sendMessage SelectNode)
+        , ((modm .|. shiftMask,               xK_n     ), sendMessage MoveNode)
+        -- NAVIGATION2D
+        -- , ((mod4Mask .|. shiftMask, xK_space),  switchLayer)
+        -- Directional navigation of screens
+        -- , ((mod4Mask,             xK_Right  ),  screenGo R False)
+        -- , ((mod4Mask,              xK_Left  ),  screenGo L False)
+        -- , ((mod4Mask,                xK_Up  ),  screenGo U False)
+        -- , ((mod4Mask,              xK_Down  ),  screenGo D False)
         ]
