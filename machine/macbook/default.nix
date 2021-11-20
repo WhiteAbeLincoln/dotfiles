a8dae408e5dd30fe25450e218016402d903e3192
Abraham White <abelincoln.white@gmail.com>
2021-11-19 22:49:18 -0700

    Fix NIX_PATH issue on darwin

--- machine/macbook/default.nix
+++ machine/macbook/default.nix
@@ -4,66 +4,11 @@
   imports = [
     <home-manager/nix-darwin>
     ../../modules/darwin
+    ../../role/darwin.nix
     ../../packages/nur
     ../../program/yabai
     ../../program/skhd
   ];
-
-
-  system = {
-    defaults-writer = {
-      "com.apple.dock" = {
-        show-recents = false;
-      };
-      "com.apple.finder" = {
-        ShowPathbar = true;
-      };
-      "com.apple.menuextra.clock" = {
-        DateFormat = "d MMM HH:mm";
-      };
-    };
-    defaults = {
-      dock.mru-spaces = false; # don't rearrange spaces by most recent use, needed for yabai
-      dock.autohide = true;
-      finder = {
-        AppleShowAllFiles = true;
-        AppleShowAllExtensions = true;
-        CreateDesktop = false; # hide desktop icons
-        _FXShowPosixPathInTitle = true;
-      };
-      trackpad = {
-        Clicking = true;
-      };
-      loginwindow = {
-        GuestEnabled = false;
-      };
-      NSGlobalDomain = {
-        ApplePressAndHoldEnabled = false; # disable accent popup on keypress
-        NSAutomaticCapitalizationEnabled = false;
-        NSAutomaticDashSubstitutionEnabled = false;
-        NSAutomaticPeriodSubstitutionEnabled = false;
-        NSAutomaticQuoteSubstitutionEnabled = false;
-        NSDocumentSaveNewDocumentsToCloud = false;
-      };
-    };
-
-    keyboard = {
-      enableKeyMapping = true;
-      remapCapsLockToEscape = true;
-    };
-
-    activationScripts.applications.text = let
-      apps = config.system.build.applications;
-    in ''
-      echo "setting up ~/Applications/Nix Apps..." >&2
-
-      if [ ! -e ~/Applications/Nix\ Apps -o -L ~/Applications/Nix\ Apps ]; then
-        ln -sfn ${apps}/Applications ~/Applications/Nix\ Apps
-      else
-        echo "warning: ~/Applications and ~/Applications/Nix Apps are directories, skipping App linking..." >&2
-      fi
-    '';
-  };
 
   nixpkgs.config = {
     allowUnfree = true;
@@ -92,16 +37,8 @@
     })
   ];
 
-  environment.systemPackages =
-    [ pkgs.vim ];
-
   services.yabai-custom.package = pkgs.yabai-binary;
   services.yabai-custom.bigSurScriptingAddition = true;
-
-  # services.nix-daemon.enable = true;
-  # users.nix.configureBuildUsers = true;
-
-  programs.zsh.enable = true;
 
   users.users.abe = {
     description = "Abraham White";
