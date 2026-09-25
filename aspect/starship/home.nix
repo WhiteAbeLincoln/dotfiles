71de415946d8318c283ab11c00701a32c4639247
Abraham White <7330103+WhiteAbeLincoln@users.noreply.github.com>
2026-09-25 12:37:42 -0600

    Remove time from shell prompt

--- aspect/starship/home.nix
+++ aspect/starship/home.nix
@@ -3,9 +3,7 @@
     enable = lib.mkDefault true;
     settings = {
       format = "$all";
-      right_format = "$time";
       command_timeout = 1250;
-      time.disabled = false;
     };
   };
 }
